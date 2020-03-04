0f3d3aa7641791ba25080520accbaa9c9ff935de
Douglas Held <douglas.held@dextra-sw.com>
2020-03-04 12:01:11 -0300

    Adicionando os aliases basicos do git

---
+++ ConfiguraGit.ps1
@@ -0,0 +1,9 @@
+git config --global alias.alias "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.rb rebase
