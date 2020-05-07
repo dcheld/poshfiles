cdc5e62b0c6708c0633f841e5edd32e56899de1e
Douglas de Campos Held <dcheld@gmail.com>
2020-05-07 17:38:05 -0300

    Adiciona o alias para a comparacao das branches
    Exibi as diferencas como: <ahead>   <behind>
    Parametro 1: Branch de destino da comparacao. Default master
    Parametro 2: Branch de origem da comparacao. Default HEAD

--- ConfiguraGit.ps1
+++ ConfiguraGit.ps1
@@ -7,3 +7,4 @@ git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.rb rebase
+git config --global alias.compare "! f() { git rev-list --left-right --count \${2-HEAD}...\${1-master}; }; f"
