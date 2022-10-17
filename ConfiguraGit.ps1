8f922e677ba494888ce81e065aae8b10417b1357
Douglas de Campos Held <douglas.held@ciandt.com>
2022-10-17 17:59:19 -0300

    Adicionar mais funções ao git

--- ConfiguraGit.ps1
+++ ConfiguraGit.ps1
@@ -1,4 +1,4 @@
-git config --global alias.alias "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
+git config --global alias.aliases "! git config --get-regexp ^alias\. | sed -e s/^alias\.// -e s/\ /\ =\ /"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
@@ -8,3 +8,4 @@ git config --global alias.last 'log -1 HEAD'
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.rb rebase
 git config --global alias.compare "! f() { git rev-list --left-right --count \${2-HEAD}...\${1-master}; }; f"
+git config --global alias.pullforce "! git stash -u; git pull; git stash pop"
