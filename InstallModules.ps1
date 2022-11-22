9faadebc7fdc233500c08d380053b1f4295c6adf
Douglas de Campos Held <douglas.held@ciandt.com>
2022-11-22 13:16:41 -0300

    Install OhMyPosh

--- InstallModules.ps1
+++ InstallModules.ps1
@@ -1,4 +1,12 @@
 #!/usr/bin/env pwsh
 
+$isWin = [System.Environment]::OSVersion.Platform -eq 'Win32NT'
+
+if ($isWin){
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+}
+else {
+    yay oh-my-posh-bin
+}
 Install-Module -Scope CurrentUser -Name posh-alias
 Install-Module -Scope CurrentUser -Name z
