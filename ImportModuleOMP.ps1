73bc397595db019ebc9a8c975ed85d25b682e336
Douglas Held <dcheld@gmail.com>
2022-12-03 21:32:29 -0300

    Load posh theme in archlinux

--- ImportModuleOMP.ps1
+++ ImportModuleOMP.ps1
@@ -1,5 +1,17 @@
 #!/usr/bin/env pwsh
 
-if (Get-Command oh-my-posh -CommandType Application -ErrorAction Ignore) {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+function GetPathForSystem() {
+    if ($isWin) {
+        return "$env:LOCALAPPDATA\Program\oh-my-posh\themes";
+    }
+    else {
+        return "/usr/share/oh-my-posh/themes";
+    }
 }
+
+if (Get-Command oh-my-posh -CommandType Application -ErrorAction Ignore) {
+    $Variable:CURRENT_THEME = ${env:POSH_THEMES_PATH} ?? $(GetPathForSystem);
+    $Variable:CURRENT_THEME = Join-Path $Variable:CURRENT_THEME "powerlevel10k_rainbow.omp.json"
+    oh-my-posh init pwsh --config $Variable:CURRENT_THEME | Invoke-Expression
+}
+
